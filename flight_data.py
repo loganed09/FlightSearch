07bd7333da76e3eb54feaf8b831cf4324cc325be
Logan <loganed09@gmail.com>
2023-10-09 16:46:58 -0400

    Update functionality and implement flight search feature

--- flight_data.py
+++ flight_data.py
@@ -1,9 +1,27 @@
-
+from datetime import datetime, timedelta
 
 class FlightData():
-    def __init__(self, data):
-        self.price = ''
-        self.departure_airport_code = '' 
-        self.departurt_city = ''
-        self.date_from = 'CMH'
-        self.date_to = ''
+    def __init__(self):
+        self.departure_airport_code = 'CMH' 
+        self.today = datetime.now()
+        self.six_months_out = self.today + timedelta(days=(30*6))
+        self.departure_city = "Columbus"
+        self.date_from = datetime.strftime(self.today, "%d/%m/%Y")
+        self.date_to = datetime.strftime(self.six_months_out, "%d/%m/%Y")
+        self.curr = 'USD'
+
+
+    def get_params(self, fly_to):
+        return {
+            "fly_from": self.departure_airport_code,
+            "fly_to": fly_to,
+            "date_from": self.date_from,
+            "date_to": self.date_to,
+            "curr": self.curr
+        }
+    
+
+
+flight_data = FlightData()
+
+print(flight_data.date_to)
--- main.py
+++ main.py
@@ -6,17 +6,19 @@ from pprint import pprint
 
 data_manager = DataManager()
 flight_search = FlightSearch()
-#flight_data = FlightData()
+flight_data = FlightData()
 
 
-flight_data = data_manager.get_data()
-pprint(flight_data)
+sheety_flight_data = data_manager.get_data()
+pprint(sheety_flight_data)
 need_codes = []
 
-for _ in range(len(flight_data)):
-    if flight_data[_]['iataCode'] == '':
-        need_codes.append(flight_data[_])
+for _ in range(len(sheety_flight_data)):
+    if sheety_flight_data[_]['iataCode'] == '':
+        need_codes.append(sheety_flight_data[_])
         flight_search.update_codes(need_codes)
-        data_manager.put_data(flight_data[_]["iataCode"], flight_data[_]["id"])
+        data_manager.put_data(sheety_flight_data[_]["iataCode"], sheety_flight_data[_]["id"])
+        #flight_search_data = FlightData(sheety_flight_data[_]["iataCode"])
+    flight_search.search_flight(flight_data.get_params(sheety_flight_data[_]["iataCode"]))
 
 
